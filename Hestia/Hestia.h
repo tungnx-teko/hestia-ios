ff978af75ba9989defcdd397b5c7357de326450c
Tung Nguyen <xtungit@gmail.com>
2020-08-14 14:26:46 +0700

    Initial Commit

---
+++ Hestia/Hestia.h
@@ -0,0 +1,19 @@
+//
+//  Hestia.h
+//  Hestia
+//
+//  Created by Tung Nguyen on 8/14/20.
+//  Copyright © 2020 Tung Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hestia.
+FOUNDATION_EXPORT double HestiaVersionNumber;
+
+//! Project version string for Hestia.
+FOUNDATION_EXPORT const unsigned char HestiaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hestia/PublicHeader.h>
+
+
